cba1743e56a6c0be5cb492c7a5f549b604db24f1
Oleksandr Hrabar <ohrabar@SECURE-LT03.eng.uwaterloo.ca>
2024-07-29 23:08:48 -0400

    created class Task

--- ece_652_final.py
+++ ece_652_final.py
@@ -7,3 +7,10 @@ def gcd(a,b):
 def lcm(a,b):
     return abs(a*b)// gcd(a,b)
 
+class Task:
+    def __init__(self,execution_time,period,deadline):
+        self.execution_time=execution_time
+        self.period=period
+        self.deadline=deadline
+        self.remaining_time=0
+        self.preemptions=0
