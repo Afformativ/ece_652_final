ee9709457f1621e572f4897cf2c140d9869af946
Oleksandr Hrabar <ohrabar@SECURE-LT03.eng.uwaterloo.ca>
2024-08-02 13:06:39 -0400

    added some comments to the code to have a final version

--- ece_652_final.py
+++ ece_652_final.py
@@ -1,27 +1,32 @@
 import sys 
 from fractions import Fraction
 
+# function to calculate the greatest common divisor
 def gcd(a,b):
     if(b==0):
         return(abs(a))
     else:
         return(gcd(b,a % b))
 
+# function to calculate the least common multiple
 def lcm(a,b):
     return abs(a*b)// gcd(a,b)
 
+# function to calculate hyperperiod
 def calculate_hyperperiod(periods):
     current_lcm=periods[0]
     for period in periods[1:]:
         current_lcm=lcm(current_lcm,period)
     return current_lcm
 
+# function to calculate the GCD of multiple numbers
 def multiple_gcd(numbers):
     current_gcd=numbers[0]
     for number in numbers[1:]:
         current_gcd=gcd(current_gcd,number)
     return current_gcd
 
+#class representing a task
 class Task:
     def __init__(self,execution_time,period,deadline):
         self.execution_time=execution_time
@@ -32,6 +37,7 @@ class Task:
         self.next_arrival_time=0
         self.absolute_deadline=0
 
+#function to read tasks from a file
 def read_task(filename):
     tasks=[]
     try:
@@ -39,52 +45,66 @@ def read_task(filename):
             for line in file:
                 try:
                     parts=line.strip().split(',')
+
+                    #exception if it is not exactly 3 elements in an input line
                     if len(parts) !=3:
-                        raise ValueError(f"Line does not contain exactly three values: {line.strip()}")
+                        raise ValueError("Line does not contain exactly three values")
                     execution_time, period, deadline=map(Fraction, parts)
 
+                    #exception if something is lower or equal to zero
                     if execution_time <=0 or period <=0 or deadline <=0:
-                        raise ValueError(f"Execution time, period and deadline must be positive integers. Found: {execution_time}, {period}, {deadline}")
+                        raise ValueError("Execution time, period and deadline must be positive integers")
+                    
+                    #exception if execution time is more than deadline/period
                     if execution_time > min(period, deadline):
-                        raise ValueError(f"execution time must be less than or equal to period and deadline. Found: {execution_time} > {min(period, deadline)}")
+                        raise ValueError("execution time must be less than or equal to period and deadline")
                     tasks.append(Task(execution_time,period,deadline))
                 except ValueError as e:
                     continue
+    # error if such file not found            
     except FileNotFoundError:
         sys.exit(1)
     return tasks
 
+#function to perform deadline monotonic scheduling
 def deadline_monotonic_scheduling(tasks,hyperperiod,precision):
     sorted_tasks=sorted(tasks,key=lambda x: x.deadline)
     time=Fraction(0)
     previous_task=None
 
     while time < hyperperiod:
+        # update task states at the start of each period
         for task in sorted_tasks:
             if time % task.period==0:
                 task.remaining_time=task.execution_time
                 task.next_arrival_time=time + task.period
                 task.absolute_deadline=time + task.deadline
         
+        # get all runnable tasks
         runnable_tasks=[t for t in sorted_tasks if t.remaining_time > 0  and time < t.absolute_deadline]
         if not runnable_tasks:
             time += precision
             continue
         
+        # select the highest priority task
         highest_priority_task=min(runnable_tasks,key=lambda t: t.deadline)
 
+        # handle preemptions
         if previous_task and previous_task != highest_priority_task:
             previous_task.preemptions += 1
         
+        #execute the highest priority task
         highest_priority_task.remaining_time -= precision
         previous_task = highest_priority_task
 
+        # check if the task has finished
         if highest_priority_task.remaining_time <= 0:
             highest_priority_task.remaining_time = 0
             previous_task=None
         
         time += precision
-
+    
+    #check if all tasks completed successfully
     for task in sorted_tasks:
         if task.remaining_time > 0:
             return 0, []
@@ -93,6 +113,8 @@ def deadline_monotonic_scheduling(tasks,hyperperiod,precision):
 
 def main(filename):
     tasks = read_task(filename)
+
+    #exception if it is not any task in the input file
     if not tasks:
         sys.exit(1)
     
@@ -111,6 +133,8 @@ def main(filename):
         print()
 
 if __name__=="__main__":
+
+    #exception if it is not file name in the terminal line
     if len(sys.argv)!=2:
         sys.exit(1)
     filename = sys.argv[1]
